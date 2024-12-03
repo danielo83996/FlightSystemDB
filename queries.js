01e8dc4617f712fd3ed1e7f7d3165dffd7b0a0d8
dancannotcode <dancannotcode@gmail.com>
2024-12-03 10:30:22 -0800

    this now has a working backend that updates the users bankaccount

--- queries.js
+++ queries.js
@@ -8,22 +8,24 @@ const queries = {
 	gender VARCHAR(10),
 	email	 VARCHAR(50)) `,
     createPaymentTable: `CREATE TABLE IF NOT EXISTS payment(
-	transactionID INT PRIMARY KEY NOT NULL,
+	transactionID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	accountID INT,
+    custID INT,
 	FOREIGN KEY(accountID) REFERENCES BANKACCOUNT(accountID),
+    FOREIGN KEY (custID) REFERENCES CUSTOMER(custID),
 	price DECIMAL(50,2) NOT NULL
 	);
 `,
     createReserveTable: `CREATE TABLE IF NOT EXISTS RESERVATION(
-    confirmation_number INT() PRIMARY KEY NOT NULL,
-    customerID INT() NOT NULL,
-    seat_number INT() NOT NULL,
-    flightID INT() NOT NULL,
-    transactionID INT() NOT NULL,
+    confirmation_number INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    customerID INT NOT NULL,
+    seat_number INT NOT NULL,
+    flightID VARCHAR(25) NOT NULL,
+    transactionID INT NOT NULL,
     reserve_date DATE NOT NULL,
-    FOREIGN KEY (customerID) REFERENCES CUSTOMER(customerID),
-    FOREIGN KEY (flightID) REFERENCES FLIGHT(flightID),
-    FOREIGN KEY (transactionID) REFERENCES PAYMENT(transactionID),
+    FOREIGN KEY (customerID) REFERENCES CUSTOMER(custID),
+    FOREIGN KEY (flightID) REFERENCES FLIGHTS(flightID),
+    FOREIGN KEY (transactionID) REFERENCES PAYMENT(transactionID)
 );`,
     createCreditCardTable: `CREATE TABLE IF NOT EXISTS credit_info(
     card_number int NOT NULL,
@@ -42,10 +44,17 @@ const queries = {
     insertCustomer: `INSERT INTO customer (Lname, Fname, DOB, pnumber, gender, email) VALUES (?, ?, ?, ?, ?, ?)`,
     insertCreditCard: `INSERT INTO credit_info(card_number, secure_number,expire_date,card_holder,card_type,accountID, custID) VALUES(?, ?, ?, ?, ?, ?, ?)`,
     insertBankAccount: `INSERT INTO bankaccount (accountID, custID, bank_name, balance) VALUES (?, ?, ?, ?)`,
+    insertReservation: `INSERT INTO RESERVATION (customerID,seat_number, flightID, transactionID, reserve_date) VALUES(?, ?, ?, ?, ?)`,
+    insertPayment: `INSERT INTO payment(accountID, custID, price) VALUES(?, ?, ?)`,
     getCustomerById: `SELECT custID FROM customer ORDER BY custID DESC LIMIT 1`, 
     getAccount: `SELECT accountID FROM bankaccount WHERE custID = ?`,
+    getBalance: `SELECT balance FROM bankaccount WHERE accountID = ? AND custID = ?`,
     updateSeatCount: 'UPDATE fly SET capacity = capacity - 1 WHERE departmentID = ? AND flightID = ?',
-    getCapacity: `SELECT capacity FROM fly WHERE departmentID = ? AND flightID = ?`
+    updateBankAccount: `UPDATE bankaccount SET balance = balance - ? WHERE accountID = ? AND custID = ?`,
+    getTransactionId: `SELECT transactionID FROM payment WHERE accountID = ? AND custID = ? ORDER BY transactionID DESC LIMIT 1`,
+    getCapacity: `SELECT capacity FROM fly WHERE departmentID = ? AND flightID = ?`,
+    getSeatPrice: `SELECT flight_price FROM FLIGHTS WHERE flightID = ? AND departmentID = ?`,
+   // getTicketInfo: `SELECT customerID, `
 
   };
   
--- repository.js
+++ repository.js
@@ -13,6 +13,16 @@ class Repository {
     const sql = queries.createPaymentTable;
     return this.dao.run(sql);
   }
+  insertPayment(accountID, custID, price){
+    return this.dao.run(queries.insertPayment, [accountID, custID, price]);
+  }
+  insertReservation(customerID,seat_number, flightID, transactionID){
+    const reserve_date = new Date().toISOString().split('T')[0];
+    return this.dao.run(queries.insertReservation, [customerID,seat_number, flightID, transactionID, reserve_date]);
+  }
+  getTransactionId(accountID, custID){
+    return this.dao.run(queries.getTransactionId, [accountID, custID])
+  }
   createReserveTable() {
     const sql = queries.createReserveTable;
     return this.dao.run(sql);
@@ -22,14 +32,20 @@ class Repository {
     return this.dao.run(sql);
   } 
   insertCreditCard(card_number, secure_number,expire_date,card_holder,card_type,accountID, custID){
-    return this.dao.run(queries.insertCreditCard, [card_number, secure_number,expire_date,card_holder,card_type,accountID, custID])
+    return this.dao.run(queries.insertCreditCard, [card_number, secure_number,expire_date,card_holder,card_type,accountID, custID]);
   }
   insertBankAccount(accountID, custID, bank_name, balance) {
     console.log(`Inserting the bankinfo`);
     return this.dao.run(queries.insertBankAccount, [accountID, custID, bank_name, balance]);
   }
   getAccount(custID){
-    return this.dao.run(queries.getAccount, [custID])
+    return this.dao.run(queries.getAccount, [custID]);
+  }
+  updateBankAccount(price, accountID, custID){
+    return this.dao.run(queries.updateBankAccount, [price, accountID,custID]);
+  }
+  getBalance(accountID, custID){
+    return this.dao.run(queries.getBalance, [accountID, custID])
   }
   insertCustomer(Lname, Fname, dob, pnumber, gender, email) {
     console.log('Inserting customer with values:', { Lname, Fname, dob, pnumber, gender, email });
@@ -48,7 +64,10 @@ class Repository {
 }
 
   updateSeatCount(departmentID, flightID){
-    return this.dao.run(queries.updateSeatCount,[departmentID, flightID])
+    return this.dao.run(queries.updateSeatCount,[departmentID, flightID]);
+  }
+  getSeatPrice(flightID, departmentID){
+    return this.dao.run(queries.getSeatPrice, [flightID, departmentID]);
   }
 }
 
